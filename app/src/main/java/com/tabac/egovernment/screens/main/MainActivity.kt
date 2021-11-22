c661aed1578581d5a5d03ae16bd08a951cb9d9c6
Tabac Egor <egor.tabac@muume.com>
2021-11-22 22:14:18 +0200

    change min sdk version

--- app/src/main/java/com/tabac/egovernment/screens/main/MainActivity.kt
+++ app/src/main/java/com/tabac/egovernment/screens/main/MainActivity.kt
@@ -26,8 +26,6 @@ class MainActivity : BaseActivity() {
             }
         }
     }
-
-
 }
 
 @Composable
--- app/src/main/java/com/tabac/egovernment/util/Logger.kt
+++ app/src/main/java/com/tabac/egovernment/util/Logger.kt
@@ -1,15 +1,15 @@
 package com.tabac.egovernment.util
 
 import timber.log.Timber
-
 fun logd(value: String) {
     Timber.d(value)
 }
-
 fun logv(value: String) {
     Timber.v(value)
 }
-
+fun loge(value: String, t: Throwable) {
+    Timber.e(value, t)
+}
 object LoggerConstants {
     const val NETWORK_LOG = "NETWORK_LOG"
 }
